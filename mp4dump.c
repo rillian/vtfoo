c641ae1ac7899b316b21fd13a7f8a6754057a17e
Ralph Giles <giles@mozilla.com>
2014-01-16 15:11:56 -0800

    Box reader method.

--- mp4dump.c
+++ mp4dump.c
@@ -9,18 +9,25 @@
 #include <stdint.h>
 #include <string.h>
 
-uint32_t read_size(FILE *in)
+typedef struct {
+  uint32_t size;
+  uint8_t type[5];
+} box;
+
+int read_size(FILE *in, uint32_t *size)
 {
   uint8_t buf[4];
   size_t read = fread(buf, 1, 4, in);
   if (read != 4) {
+    /* TODO: handle extended size */
     fprintf(stderr, "Error: couldn't read size\n");
-    return 0;
+    return -1;
   }
-  return buf[0] << 24 |
-         buf[1] << 16 |
-         buf[2] <<  8 |
-         buf[3];
+  *size = buf[0] << 24 |
+          buf[1] << 16 |
+          buf[2] <<  8 |
+          buf[3];
+  return 0;
 }
 
 int read_type(FILE *in, uint8_t *type)
@@ -31,8 +38,24 @@ int read_type(FILE *in, uint8_t *type)
     fprintf(stderr, "Error: couldn't read type\n");
     return -1;
   }
+  /* TODO: handle uuid types */
   memcpy(type, buf, 4);
   type[4] = '\0';
+  return 0;
+}
+
+int read_box(FILE *in, box *box)
+{
+  int ret;
+
+  if (!box)
+    return -8;
+
+  ret = read_size(in, &box->size);
+  if (ret) return ret;
+  ret = read_type(in, box->type);
+  if (ret) return ret;
+
   return 0;
 }
 
@@ -50,7 +73,8 @@ int dump_ftyp(FILE *in, uint32_t size)
     fprintf(stderr, "Error: couldn't read brand\n");
     return -1;
   }
-  version = read_size(in);
+  if (read_size(in, &version))
+      return -2;
   fprintf(stdout, "   %s version %u\n", brand, version);
   if (brands > 0) {
     fprintf(stdout, "   compatible with:");
@@ -67,28 +91,27 @@ int dump_ftyp(FILE *in, uint32_t size)
 /* dump a file */
 int dump(FILE *in)
 {
+  int ret;
   while (!feof(in)) {
-    /* read size */
-    uint32_t size = read_size(in);
-    uint8_t type[5];
-    if (size < 8) {
+    box box;
+    ret = read_box(in, &box);
+    if (ret) {
+      fprintf(stderr, "Error: couldn't read box header\n");
+      return ret;
+    }
+    if (box.size < 8) {
       fprintf(stderr, "Error: invalid box size\n");
       return -1;
     }
-    /* read type */
-    if (read_type(in, type)) {
-      fprintf(stderr, "Error: couldn't read box type\n");
-      return -2;
-    }
     /* report box */
-    fprintf(stdout, " '%s' box %u bytes\n", type, size);
+    fprintf(stdout, " '%s' box %u bytes\n", box.type, box.size);
     /* dispatch to per-type parsers */
-    if (!memcmp(type, "ftyp", 4)) {
-      dump_ftyp(in, size);
+    if (!memcmp(box.type, "ftyp", 4)) {
+      dump_ftyp(in, box.size);
     }
     else {
       /* skip to the next box */ 
-      fseek(in, size - 8, SEEK_CUR);
+      fseek(in, box.size - 8, SEEK_CUR);
     }
   }
   return 0;
