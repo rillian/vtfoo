b0ec17da3cc35629cbf8dc548622618e18a5390b
Ralph Giles <giles@mozilla.com>
2014-01-16 12:12:33 -0800

    Dump top level boxes.
    We error out at the end instead of realizing we're at the end, I think.

--- mp4dump.c
+++ mp4dump.c
@@ -6,9 +6,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+uint32_t read_size(FILE *in)
+{
+  uint8_t buf[4];
+  size_t read = fread(buf, 1, 4, in);
+  if (read != 4) {
+    fprintf(stderr, "Error: couldn't read size\n");
+    return 0;
+  }
+  return buf[0] << 24 |
+         buf[1] << 16 |
+         buf[2] <<  8 |
+         buf[3];
+}
 
 int dump(FILE *in)
 {
+  while (!feof(in)) {
+    uint32_t size = read_size(in);
+    uint8_t type[5];
+    size_t read;
+    if (size < 8) {
+      fprintf(stderr, "Error: invalid box size\n");
+      return -1;
+    }
+    read = fread(type, 1, 4, in);
+    if (read != 4) {
+      fprintf(stderr, "Error: couldn't read type\n");
+      return -2;
+    }
+    type[4] = '\0';
+    fprintf(stderr, " '%s' box %u bytes\n", type, size);
+    /* skip to the next box */ 
+    fseek(in, size - 8, SEEK_CUR);
+  }
   return 0;
 }
 
